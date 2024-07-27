41ff1aa073f0262d6fdc803b2b053c32ad9fdaa4
Prabhakar Mishra <157229277+prabhu123mishra@users.noreply.github.com>
2024-07-27 11:28:30 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(typeof null);  // type of null is object
+console.log(typeof undefined); //type of undefined is undefined
+
+
+// null => standlone value
+//jese hm server se puche temperature kya h aaj ka toh agr server ke pass nhi hoga toh wo null return krega..
+//undefined mtlb not assign any value 
