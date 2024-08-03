4b5831c1b6db3c5d23f503d725433d0ed3040127
Prabhakar Mishra <157229277+prabhu123mishra@users.noreply.github.com>
2024-08-03 20:08:09 +0000

    object

---
+++ basics/object.js
@@ -0,0 +1,11 @@
+const jsUser = {
+    name: "prabhu",
+    age: 23,
+    email:"prabhu1@google.com",
+    location:"kolkata",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","tuesday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["location"])
